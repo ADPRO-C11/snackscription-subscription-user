f71e07f77675153cb9cf74cf9c36ab1a2fefcb37
lontonggg <zadareyhan@gmail.com>
2024-04-23 16:31:50 +0700

    Initial Codebase

---
+++ src/test/java/snackscription/subscription/SubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package snackscription.subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
