06f4f5cd229c15e33fa2a349b45a5a895c6291f8
lontonggg <zadareyhan@gmail.com>
2024-04-24 12:25:17 +0700

    [FIX] Fixed Controller

--- src/main/java/snackscription/subscription/controller/SubscriptionController.java
+++ src/main/java/snackscription/subscription/controller/SubscriptionController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("")
+@RequestMapping("/")
 public class SubscriptionController {
-    @GetMapping("")
+    @GetMapping("/subscription")
     public String subscriptionPage(){
         return "subscriptionPage";
     }
