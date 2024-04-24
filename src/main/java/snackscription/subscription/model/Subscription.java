96d1c02d46bbe2c181645e64502099a2797b7a88
lontonggg <zadareyhan@gmail.com>
2024-04-24 19:58:06 +0700

    [GREEN] Implement Subscription Model

--- src/main/java/snackscription/subscription/model/Subscription.java
+++ src/main/java/snackscription/subscription/model/Subscription.java
@@ -3,8 +3,10 @@ package snackscription.subscription.model;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import snackscription.subscription.enums.SubscriptionStatus;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -18,10 +20,28 @@ public class Subscription {
     LocalDateTime dateCreated;
 
     public Subscription(){
-
+        this.id = UUID.randomUUID().toString();
     }
 
     public Subscription(String uniqueCode, String userId, String subscriptionBoxId, ShippingAddress shippingAddress, String status){
+        this.id = UUID.randomUUID().toString();
 
+        if (!uniqueCode.startsWith("MTH-") && !uniqueCode.startsWith("QTR-") && !uniqueCode.startsWith("SAA-")) {
+            throw new IllegalArgumentException("Invalid unique code");
+        }
+
+        this.uniqueCode = uniqueCode;
+        this.userId = userId;
+        this.subscriptionBoxId = subscriptionBoxId;
+        this.shippingAddress = shippingAddress;
+        this.setStatus(status);
+    }
+
+    public void setStatus(String status){
+        if (SubscriptionStatus.contains(status)) {
+            this.status = status;
+        } else{
+            throw new IllegalArgumentException("Invalid status");
+        }
     }
 }
