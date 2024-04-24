742e19460ff76dab8c5d41c923e0d49156ae0ee8
lontonggg <zadareyhan@gmail.com>
2024-04-24 20:55:05 +0700

    [REFACTOR] Refactor ShippingAddress to be embedded into Subscription Entity

--- src/main/java/snackscription/subscription/model/ShippingAddress.java
+++ src/main/java/snackscription/subscription/model/ShippingAddress.java
@@ -1,8 +1,10 @@
 package snackscription.subscription.model;
 
+import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.Setter;
 
+@Embeddable
 @Getter
 @Setter
 public class ShippingAddress {
