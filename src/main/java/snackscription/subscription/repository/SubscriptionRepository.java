47874bc9de995b1f84d2ea838b6c04059b46caec
lontonggg <zadareyhan@gmail.com>
2024-04-25 13:56:08 +0700

    [RED] Create SubscriptionRepository Skeleton

--- src/main/java/snackscription/subscription/repository/SubscriptionRepository.java
+++ src/main/java/snackscription/subscription/repository/SubscriptionRepository.java
@@ -1,28 +1,40 @@
 package snackscription.subscription.repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import snackscription.subscription.model.Subscription;
-import snackscription.subscription.service.SubscriptionService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class SubscriptionRepository {
-    private List<Subscription> subscriptionData;
+    @Autowired
+    private EntityManager entityManager;
 
+    @Transactional
+    public Subscription save(Subscription subscription) {
+        return null;
+    }
+
+    @Transactional
     public List<Subscription> findAll(){
-        return subscriptionData;
-    }
-
-    public Subscription findById(String id){
         return null;
     }
 
-    public Subscription create(){
+    @Transactional
+    public Optional<Subscription> findById(String id){
         return null;
     }
 
-    public Subscription edit(String id){
+    @Transactional
+    public Subscription update(Subscription subscription){
         return null;
+    }
+
+    @Transactional
+    public void delete(String id){
     }
 }
