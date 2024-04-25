fae48bbe28b074608c09f303b54a40385450d70f
lontonggg <zadareyhan@gmail.com>
2024-04-25 18:12:51 +0700

    [RED] Create SubscriptionController Skeleton

--- src/main/java/snackscription/subscription/controller/SubscriptionController.java
+++ src/main/java/snackscription/subscription/controller/SubscriptionController.java
@@ -1,14 +1,46 @@
 package snackscription.subscription.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import snackscription.subscription.dto.SubscriptionDTO;
+import snackscription.subscription.model.Subscription;
+import snackscription.subscription.service.SubscriptionService;
 
-@Controller
-@RequestMapping("/")
+import java.util.List;
+
+@RestController
+@RequestMapping("/subscription")
 public class SubscriptionController {
-    @GetMapping("/subscription")
-    public String subscriptionPage(){
-        return "subscriptionPage";
+
+    private final SubscriptionService subscriptionService;
+
+    public SubscriptionController (SubscriptionService subscriptionService) {
+        this.subscriptionService = subscriptionService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Subscription> create(@RequestBody SubscriptionDTO subscriptionDTO){
+        return null;
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<SubscriptionDTO>> findAll(){
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SubscriptionDTO> findById(@PathVariable String id){
+        return null;
+    }
+
+    @PatchMapping("/update")
+    public ResponseEntity<Subscription> update(@RequestBody SubscriptionDTO subscriptionDTO){
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable String id){
+        return null;
     }
 }
