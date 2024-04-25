ec2fe2ae3e4dae7f54f717b03604d51302aa9e04
lontonggg <zadareyhan@gmail.com>
2024-04-25 17:16:09 +0700

    [GREEN] Implements SubscriptionRepository

--- src/main/java/snackscription/subscription/repository/SubscriptionRepository.java
+++ src/main/java/snackscription/subscription/repository/SubscriptionRepository.java
@@ -1,6 +1,7 @@
 package snackscription.subscription.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -16,25 +17,32 @@ public class SubscriptionRepository {
 
     @Transactional
     public Subscription save(Subscription subscription) {
-        return null;
+        entityManager.persist(subscription);
+        return subscription;
     }
 
     @Transactional
     public List<Subscription> findAll(){
-        return null;
+        String jpql = "SELECT s FROM Subscription s";
+        TypedQuery<Subscription> query = entityManager.createQuery(jpql, Subscription.class);
+        return query.getResultList();
     }
 
     @Transactional
     public Optional<Subscription> findById(String id){
-        return null;
+        Subscription subscription = entityManager.find(Subscription.class, id);
+        return Optional.ofNullable(subscription);
     }
 
     @Transactional
     public Subscription update(Subscription subscription){
-        return null;
+        return entityManager.merge(subscription);
     }
 
     @Transactional
     public void delete(String id){
+        Subscription subscription = findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Subscription not found"));
+        entityManager.remove(subscription);
     }
 }
