c8062613dc57a72047b43b21cb31a96d56d6969b
lontonggg <zadareyhan@gmail.com>
2024-05-01 04:17:44 +0700

    [FIX] Added better error handling and error response in Controller

--- src/main/java/snackscription/subscription/controller/SubscriptionController.java
+++ src/main/java/snackscription/subscription/controller/SubscriptionController.java
@@ -8,10 +8,11 @@ import snackscription.subscription.model.Subscription;
 import snackscription.subscription.service.SubscriptionService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/subscription")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class SubscriptionController {
 
     private final SubscriptionService subscriptionService;
@@ -22,7 +23,13 @@ public class SubscriptionController {
 
     @PostMapping("/create")
     public ResponseEntity<Subscription> create(@RequestBody SubscriptionDTO subscriptionDTO){
-        return new ResponseEntity<>(subscriptionService.save(subscriptionDTO), HttpStatus.CREATED);
+        Subscription subscription;
+        try {
+            subscription = subscriptionService.save(subscriptionDTO);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(subscription);
     }
 
     @GetMapping("/list")
@@ -32,17 +39,57 @@ public class SubscriptionController {
 
     @GetMapping("/{id}")
     public ResponseEntity<SubscriptionDTO> findById(@PathVariable String id){
-        return new ResponseEntity<>(subscriptionService.findById(id), HttpStatus.OK);
+        try {
+            UUID.fromString(id);
+        } catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+
+        SubscriptionDTO subscription;
+        try {
+            subscription = subscriptionService.findById(id);
+        } catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(subscription);
     }
 
     @PatchMapping("/update")
     public ResponseEntity<Subscription> update(@RequestBody SubscriptionDTO subscriptionDTO){
-        return new ResponseEntity<>(subscriptionService.update(subscriptionDTO), HttpStatus.OK);
+        Subscription subscription;
+
+        if(subscriptionDTO.getId() == null || subscriptionDTO.getId().isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        try{
+            subscriptionService.findById(subscriptionDTO.getId());
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+
+        try {
+            subscription = subscriptionService.update(subscriptionDTO);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(subscription);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> delete(@PathVariable String id){
-        subscriptionService.delete(id);
+        try {
+            UUID.fromString(id);
+        } catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+
+        try {
+            subscriptionService.delete(id);
+        } catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+
         return new ResponseEntity<>("DELETE SUCCESS", HttpStatus.OK);
     }
 }
