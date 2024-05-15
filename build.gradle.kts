ad12e35ddc6d8f6676a1acda66616796f58971fd
lontonggg <zadareyhan@gmail.com>
2024-05-16 00:00:48 +0700

    [MONITORING] Add grafana and prometheus for monitoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,8 @@ dependencies {
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	runtimeOnly("io.micrometer:micrometer-registry-prometheus:1.12.5")
+	implementation("org.springframework.boot:spring-boot-starter-actuator:3.2.5")
 }
 
 tasks.withType<Test> {
