9c45698db2bee7f1d565b53ccfba0190fd63f956
lontonggg <zadareyhan@gmail.com>
2024-05-23 16:32:59 +0700

    [REFACTOR] Implement authentication and authorization

--- src/test/java/snackscription/subscription/controller/SubscriptionControllerTest.java
+++ src/test/java/snackscription/subscription/controller/SubscriptionControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import snackscription.subscription.dto.SubscriptionDTO;
 import snackscription.subscription.model.Subscription;
 import snackscription.subscription.service.SubscriptionService;
+import snackscription.subscription.utils.JWTUtils;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,11 +25,15 @@ class SubscriptionControllerTest {
     @Mock
     private SubscriptionService subscriptionService;
 
+    @Mock
+    private JWTUtils jwtUtils;
+
     @InjectMocks
     private SubscriptionController subscriptionController;
 
     private SubscriptionDTO subscriptionDTO;
     private Subscription subscription;
+    private final String validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyMTIzIiwicm9sZSI6IlVTRVIiLCJpYXQiOjE3MTYyMzkwMjIsImV4cCI6MTcxNjMyNTQyMn0.5wT-7Q_Oc2by1b2q6xR6g-eOdAG_RUVH5HbyCbn88mE";
 
     @BeforeEach
     void setUp() {
@@ -47,14 +52,16 @@ class SubscriptionControllerTest {
         subscription.setUserId("user123");
         subscription.setSubscriptionBoxId("box123");
         subscription.setStatus("PENDING");
+
+        when(jwtUtils.isTokenValid(validToken)).thenReturn(true);
     }
 
     @Test
-    void testCreate() {
+    void testCreate() throws IllegalAccessException {
         when(subscriptionService.save(any(SubscriptionDTO.class)))
                 .thenReturn(CompletableFuture.completedFuture(subscription));
 
-        CompletableFuture<ResponseEntity<Subscription>> result = subscriptionController.create(subscriptionDTO);
+        CompletableFuture<ResponseEntity<Subscription>> result = subscriptionController.create(validToken, subscriptionDTO);
 
         assertNotNull(result);
         assertTrue(result.isDone());
@@ -62,13 +69,13 @@ class SubscriptionControllerTest {
     }
 
     @Test
-    void testFindAll() {
+    void testFindAll() throws IllegalAccessException {
         List<SubscriptionDTO> subscriptionDTOList = List.of(subscriptionDTO);
 
         when(subscriptionService.findAll())
                 .thenReturn(CompletableFuture.completedFuture(subscriptionDTOList));
 
-        CompletableFuture<ResponseEntity<List<SubscriptionDTO>>> result = subscriptionController.findAll();
+        CompletableFuture<ResponseEntity<List<SubscriptionDTO>>> result = subscriptionController.findAll(validToken);
 
         assertNotNull(result);
         assertTrue(result.isDone());
@@ -76,13 +83,13 @@ class SubscriptionControllerTest {
     }
 
     @Test
-    void testUpdate() {
+    void testUpdate() throws IllegalAccessException {
         when(subscriptionService.findById(subscriptionDTO.getId()))
                 .thenReturn(CompletableFuture.completedFuture(Optional.of(subscriptionDTO)));
         when(subscriptionService.update(subscriptionDTO))
                 .thenReturn(CompletableFuture.completedFuture(subscription));
 
-        CompletableFuture<ResponseEntity<Subscription>> result = subscriptionController.update(subscriptionDTO);
+        CompletableFuture<ResponseEntity<Subscription>> result = subscriptionController.update(validToken, subscriptionDTO);
 
         assertNotNull(result);
         assertTrue(result.isDone());
@@ -90,11 +97,11 @@ class SubscriptionControllerTest {
     }
 
     @Test
-    void testFindById() {
+    void testFindById() throws IllegalAccessException {
         when(subscriptionService.findById(subscriptionDTO.getId()))
                 .thenReturn(CompletableFuture.completedFuture(Optional.of(subscriptionDTO)));
 
-        CompletableFuture<ResponseEntity<SubscriptionDTO>> result = subscriptionController.findById(subscriptionDTO.getId());
+        CompletableFuture<ResponseEntity<SubscriptionDTO>> result = subscriptionController.findById(validToken, subscriptionDTO.getId());
 
         assertNotNull(result);
         assertTrue(result.isDone());
@@ -102,10 +109,10 @@ class SubscriptionControllerTest {
     }
 
     @Test
-    void testFindByIdInvalidId() {
+    void testFindByIdInvalidId() throws IllegalAccessException {
         String invalidId = "invalid_id";
 
-        CompletableFuture<ResponseEntity<SubscriptionDTO>> result = subscriptionController.findById(invalidId);
+        CompletableFuture<ResponseEntity<SubscriptionDTO>> result = subscriptionController.findById(validToken, invalidId);
 
         assertNotNull(result);
         assertTrue(result.isDone());
@@ -113,11 +120,11 @@ class SubscriptionControllerTest {
     }
 
     @Test
-    void testDelete() {
+    void testDelete() throws IllegalAccessException {
         when(subscriptionService.delete(subscriptionDTO.getId()))
                 .thenReturn(CompletableFuture.completedFuture(null));
 
-        CompletableFuture<ResponseEntity<String>> result = subscriptionController.delete(subscriptionDTO.getId());
+        CompletableFuture<ResponseEntity<String>> result = subscriptionController.delete(validToken, subscriptionDTO.getId());
 
         assertNotNull(result);
         assertTrue(result.isDone());
@@ -125,48 +132,48 @@ class SubscriptionControllerTest {
     }
 
     @Test
-    void testUpdate_InvalidId() {
+    void testUpdate_InvalidId() throws IllegalAccessException {
         SubscriptionDTO invalidSubscriptionDTO = new SubscriptionDTO();
         CompletableFuture<ResponseEntity<Subscription>> expectedResult = CompletableFuture.completedFuture(ResponseEntity.badRequest().build());
 
-        CompletableFuture<ResponseEntity<Subscription>> result = subscriptionController.update(invalidSubscriptionDTO);
+        CompletableFuture<ResponseEntity<Subscription>> result = subscriptionController.update(validToken, invalidSubscriptionDTO);
 
         assertTrue(result.isDone());
         assertEquals(expectedResult.join(), result.join());
     }
 
     @Test
-    void testUpdate_SubscriptionNotFound() {
+    void testUpdate_SubscriptionNotFound() throws IllegalAccessException {
         SubscriptionDTO invalidSubscriptionDTO = new SubscriptionDTO();
         invalidSubscriptionDTO.setId(UUID.randomUUID().toString());
         CompletableFuture<ResponseEntity<Subscription>> expectedResult = CompletableFuture.completedFuture(ResponseEntity.notFound().build());
 
         when(subscriptionService.findById(invalidSubscriptionDTO.getId())).thenReturn(CompletableFuture.completedFuture(Optional.empty()));
 
-        CompletableFuture<ResponseEntity<Subscription>> result = subscriptionController.update(invalidSubscriptionDTO);
+        CompletableFuture<ResponseEntity<Subscription>> result = subscriptionController.update(validToken, invalidSubscriptionDTO);
 
         assertTrue(result.isDone());
         assertEquals(expectedResult.join(), result.join());
     }
 
     @Test
-    void testDelete_InvalidId() {
+    void testDelete_InvalidId() throws IllegalAccessException {
         CompletableFuture<ResponseEntity<String>> expectedResult = CompletableFuture.completedFuture(ResponseEntity.badRequest().build());
 
-        CompletableFuture<ResponseEntity<String>> result = subscriptionController.delete("invalid_id");
+        CompletableFuture<ResponseEntity<String>> result = subscriptionController.delete(validToken, "invalid_id");
 
         assertTrue(result.isDone());
         assertEquals(expectedResult.join(), result.join());
     }
 
     @Test
-    void testDelete_SubscriptionNotFound() {
+    void testDelete_SubscriptionNotFound() throws IllegalAccessException {
         String invalidId = UUID.randomUUID().toString();
         CompletableFuture<ResponseEntity<String>> expectedResult = CompletableFuture.completedFuture(ResponseEntity.notFound().build());
 
         when(subscriptionService.delete(invalidId)).thenReturn(CompletableFuture.failedFuture(new IllegalArgumentException()));
 
-        CompletableFuture<ResponseEntity<String>> result = subscriptionController.delete(invalidId);
+        CompletableFuture<ResponseEntity<String>> result = subscriptionController.delete(validToken, invalidId);
 
         assertTrue(result.isDone());
         assertEquals(expectedResult.join(), result.join());
@@ -179,14 +186,14 @@ class SubscriptionControllerTest {
     }
 
     @Test
-    void testCreateExceptionHandler() {
+    void testCreateExceptionHandler() throws IllegalAccessException {
         SubscriptionDTO invalidSubscriptionDTO = new SubscriptionDTO();
         CompletableFuture<ResponseEntity<Subscription>> expectedResult = CompletableFuture.completedFuture(ResponseEntity.badRequest().build());
 
         CompletableFuture<Subscription> failedFuture = CompletableFuture.failedFuture(new IllegalArgumentException("Invalid Request"));
         when(subscriptionService.save(invalidSubscriptionDTO)).thenReturn(failedFuture);
 
-        CompletableFuture<ResponseEntity<Subscription>> result = subscriptionController.create(invalidSubscriptionDTO);
+        CompletableFuture<ResponseEntity<Subscription>> result = subscriptionController.create(validToken, invalidSubscriptionDTO);
 
         assertTrue(result.isDone());
         assertEquals(expectedResult.join(), result.join());
