8d71f7e0afaa303caa529c43d823602d09065b5d
lontonggg <zadareyhan@gmail.com>
2024-05-24 18:01:27 +0700

    [FIX] Disable CSRF protection

--- src/main/java/snackscription/subscription/config/SecurityConfig.java
+++ src/main/java/snackscription/subscription/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package snackscription.subscription.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,7 +23,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(authorizeRequests ->
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+            .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests.requestMatchers("/subscription/**", "/public/**").permitAll()
                                 .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
